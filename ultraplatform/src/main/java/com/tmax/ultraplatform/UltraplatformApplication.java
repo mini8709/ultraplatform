cea436b58dd286e02071a49e2586456e1f25346d
Youngmin <mini8709@naver.com>
2021-08-05 16:47:42 +0900

    feat: Initialize project

---
+++ ultraplatform/src/main/java/com/tmax/ultraplatform/UltraplatformApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.ultraplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltraplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltraplatformApplication.class, args);
+	}
+
+}
---
+++ ultraplatform/src/test/java/com/tmax/ultraplatform/UltraplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.ultraplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
