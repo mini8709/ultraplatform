1e0605a5f1ee9752fae8be50ee10ac9e424bf934
jinyoung_kim2 <jinyoung_kim2@tmax.co.kr>
2021-08-13 14:36:27 +0900

    ADd findByCategory / ProductService

--- ultraplatform/src/main/java/com/tmax/ultraplatform/repository/ProductRepository.java
+++ ultraplatform/src/main/java/com/tmax/ultraplatform/repository/ProductRepository.java
@@ -3,6 +3,5 @@ package com.tmax.ultraplatform.repository;
 import com.tmax.ultraplatform.domain.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 public interface ProductRepository extends JpaRepository<Product,Long> {
 }
